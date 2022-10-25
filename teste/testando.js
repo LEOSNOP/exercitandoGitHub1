0cb0bf434f4628f89914b2bb0d9850302ebc8e67
_L_ <conceicao.leonardo9@gmail.com>
2022-10-25 11:59:36 -0300

    testando.js2

--- teste/testando.js
+++ teste/testando.js
@@ -6,8 +6,16 @@ function soma (a , b){
 
 }
 
-var res = soma(1, 4)
+function verifica(r){
+    if(r %2 == 0){
+        console.log("numero par")
+    }else{
+        console.log("numero impar")
+    }
+}
 
+var res = soma(1, 4)
+verifica(res)
 console.log(res)
 
 
